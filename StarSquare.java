3543a319140cd835706760cf0cf0d5cb2f6665d1
arakelyanm <marakelyan@york.citycollege.eu>
2024-10-28 15:42:41 +0200

    Add files via upload

---
+++ StarSquare.java
@@ -0,0 +1,5 @@
+public class StarSquare{
+    public static void main(String args[]){
+        System.out.println("*******\n*******\n*******\n*******\n*******\n*******");
+    }
+}
---
+++ StarSquareGUI.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class StarSquareGUI{
+    public static void main(String args[]){
+        JOptionPane.showMessageDialog(null, "*******\n*******\n*******\n*******\n*******");
+    }
+}   
